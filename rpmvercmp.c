dc8a041d867e7b900d2a1221d1ca8127d78e91cc
Paweł Gajda <mis@pld-linux.org>
2000-11-10 19:01:06 +0000

    - very simple but useful tool

---
+++ rpmvercmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <rpm/rpmlib.h>
+#include <rpm/misc.h>
+
+int main(int argc, char *argv[])
+{
+    int cmprc;
+    
+    if (argc != 3) {
+        printf("Usage: rpmvercmp [VERSION1] [VERSION2]\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    cmprc = rpmvercmp(argv[1], argv[2]);
+    printf("%s %s %s\n", argv[1], cmprc == 0 ?  "==" : cmprc > 0 ? ">" : "<",
+           argv[2]);
+    exit(cmprc);
+}
