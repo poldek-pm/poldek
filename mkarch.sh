7b5419427307237b0c84b959ac8ffb991838a4ad
Paweł Gajda <mis@pld-linux.org>
2000-11-24 19:37:23 +0000

    - fixes

--- mkarch.sh
+++ mkarch.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
+
+CPDIR=/z
 make -f Makefile.extra mclean
-make -f Makefile.extra backup cparch=1 backupdir=/z/
+
+if [ -w $CPDIR ]; then
+    make -f Makefile.extra backup cparch=1 backupdir=$CPDIR
+else 
+    make -f Makefile.extra backup
+fi
