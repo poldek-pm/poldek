9c9e685fd23d67f6d91e5adb6debd601337506be
Paweł Gajda <mis@pld-linux.org>
2002-02-21 23:52:34 +0000

    - PLD CDs poldek helper script

---
+++ cdpoldek.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+# $Id$
+# Put this script on the first disk of dist set
+# 
+
+PATH="/bin/:/usr/bin:/sbin:/usr/sbin"
+
+whereiam=$(dirname $0)
+
+if [ "${whereiam}." = ".." ]; then
+    whereiam=$(pwd)
+fi
+
+cmd=poldek
+
+if [ -f "${whereiam}/.poldekrc" ]; then
+    cmd="$cmd --conf ${whereiam}/.poldekrc"
+fi
+
+if [ -f "${whereiam}/.poldekpri.conf" ]; then
+    cmd="$cmd --priconf ${whereiam}/.poldekpri.conf"
+fi
+
+VF_MNTPOINT="$whereiam"
+export VF_MNTPOINT
+$cmd $@
