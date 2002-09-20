310b0e498fbb4a65827c2dbc54a2b8365c8c8cd9
Paweł Gajda <mis@pld-linux.org>
2002-09-20 15:39:28 +0000

    - added poldek's logo (picture by Karol Krenski <mimooh@sgsp.edu.pl> - thanks :-))

--- mkhtmltoc.pl
+++ mkhtmltoc.pl
@@ -9,7 +9,13 @@ my @hdr_lines = ();
 my @lines = ();
 my $hdr = 1;
 
+
+
 while (<>) {
+    s/<H1>\s+the\s+poldek//;
+    s/<BODY>/<BODY BGCOLOR="#FFFFFF">/; 
+    $_ = qq{<a name="logo_info"></a> $_} if /to\s+Karol\s+Krenski/;
+    
     if (m|NAME="(s\d+)">\d+\.\s*(.+)</A>|) {
 	$hdr = 0;
 	push @toc, {
@@ -25,7 +31,12 @@ while (<>) {
     }	
 }
 
-my $tochtml = q{<br><p><ol>};
+my $tochtml = q{
+<div align="center" style="font-size: xx-small">
+<img border="0" src="logo.png" alt="poldek's logo (C) Karol Krenski (mimooh at sgsp.edu.pl)">
+<a href="#logo_info">*)</a>
+</div>
+<br><p><ol>};
 foreach (@toc) {
     #print "$_->{n} $_->{l}\n";
     $tochtml .= qq{
@@ -39,4 +50,18 @@ print $tochtml;
 
 foreach (@lines) {
     print $_;
+}
+
+# NFY
+sub thanks_to {
+    open(F,  "<NEWS") || die;
+    while (<F>) {
+	if (/(<.+@.+?>)/) {
+	    my ($name, $sname, $email) = /(.+?)\s+(.+?)\s+(<.+?@.+?>)/;
+	    if ($name && $sname && $email) {
+		print STDERR "$name, $sname, $email\n";
+	    }
+	}
+    }
+    close(F);
 }
