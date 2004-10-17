b740143bde6e703ccefe8dccd308964c102e2dc2
Paweł Gajda <mis@pld-linux.org>
2004-10-17 19:02:24 +0000

    - --with poldekuser param to spec

--- userpoldekadd.sh
+++ userpoldekadd.sh
@@ -1,9 +1,50 @@
 #!/bin/sh
+# $Id$
+# See help() for description
 
 PATH="/bin:/sbin:/usr/bin:/usr/sbin"
-USER=poldek
+USER=${POLDEKUSER:-"poldek"}
 SYSCONFDIR="/etc"
 SUDOERS="$SYSCONFDIR/sudoers"
+
+help() {
+    echo "Script configures poldek to run as ordinary user when executed by root:"
+    echo " - adds user '$USER'"
+    echo " - adds to sudoers file permission to '$USER' to run rpm as root"
+    echo "   without a password"
+    echo " - adds 'runas = $USER' to poldek.conf"
+    echo " - copy existing poldek files (/root/.poldekrc and so on) to '$USER' user $HOME"
+    echo ""
+    echo "Do '$0 --yes' to configure"
+}
+
+copyfile() {
+    file=$1
+    dir=$2
+
+    if [ ! -r $HOME/$file ]; then
+        return 1
+    fi
+
+    if [ -r $dir/$file ]; then
+        echo "$dir/$file exists, $HOME/$file not copied"
+        return 1
+    else
+        echo "Copying $HOME/$file to $dir/..."
+        cp -a $HOME/$file $dir && chown -R $USER.$USER $dir/$file
+    fi
+}
+
+if [ "$(id -u 2>/dev/null)" != "0" ]; then
+    echo "You must be root to run me"
+    exit 1
+fi
+
+ARG=$1
+if [ -z "$ARG" -o "$ARG" != "--yes" ]; then
+    help
+    exit 0
+fi        
 
 if [ ! -f $SUDOERS ]; then
     SUDOERS="/sudo/sudoers"
@@ -14,21 +55,26 @@ if [ ! -f $SUDOERS ]; then
     exit 1
 fi 
 
+if [ -n "${POLDEKHOME}" ]; then
+    homedir="${POLDEKHOME}"
 
-homedir=/var/cache
-if [ ! -d $homedir ]; then homedir=/var/lib; fi
-if [ ! -d $homedir ]; then 
-    echo "$homedir: no such directory (non-FHS file system?)"
-    exit
-fi 
+else
+    homedir="/var/cache"
+    if [ ! -d $homedir ]; then 
+        echo "$homedir: no such directory (non-FHS file system?)"
+        exit 1
+    fi 
+    homedir="$homedir/$USER"
+fi
 
-homedir="$homedir/$USER"
-mkdir -p -v --mode=700 $homedir || exit 1
+if [ ! -d $homedir ]; then
+    mkdir -p -v --mode=755 $homedir || exit 1
+fi
 
 if [ -z "$(id -u $USER 2>/dev/null)" ]; then
-    echo "Adding user '$USER.$USER' with $homedir HOME"
+    echo "Adding user '$USER.$USER' with home directory '$homedir'"
     groupadd -r -f $USER || exit 1
-    useradd -r -d $homedir poldek -g poldek -s /bin/sh -c "poldek user" || exit 1
+    useradd -r -d $homedir $USER -g $USER -s /bin/sh -c "poldek user" || exit 1
 fi
 
 chown -R $USER.$USER $homedir
@@ -42,4 +88,29 @@ if ! grep -q "^poldek" $SUDOERS; then
     echo "Notice: $SUDOERS file changed"
 fi
 
-echo "Remember to add line 'runas = $USER' to poldek configuration file!"
+if copyfile .poldekrc "$homedir"; then
+    copyfile .poldek_history $homedir
+    copyfile .poldek-cache $homedir
+    copyfile .poldek-aliases.conf $homedir
+fi    
+homeconfig="$HOME/.poldekrc"
+if [ -f $homeconfig ]; then
+    echo "$homeconfig saved as ${homeconfig}.save"
+    mv -f $homeconfig ${homeconfig}.save
+fi
+
+config="/etc/poldek/poldek.conf";
+if [ ! -f $config ]; then config="/etc/poldek.conf"; fi
+
+if [ -f $config ]; then
+    if ! grep -q "^[ ]*runas[ ]*=[ ]*$USER" $config; then
+        echo "Configure $config poldek to run as '$USER'..."
+        perl -pi -e "s|^\#?\s*runas\s*=\s*\w+\s*|runas = $USER\n|" $config
+    fi
+elif [ -z "${UNDERRPM}" ]; then
+    echo "Cannot found poldek.conf file, modify it yourself adding:"
+    echo "runas = $USER"
+fi
+    
+
+exit 0
