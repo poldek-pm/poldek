50048e58de3224b757b151fdaced7ada52446f77
Paweł Gajda <mis@pld-linux.org>
2004-10-17 19:18:14 +0000

    - clearenv() stuff

--- cli/su.c
+++ cli/su.c
@@ -1,22 +1,22 @@
-/* Stolen from su */
 /*
-  su for GNU.  Run a shell with substitute user and group IDs.
-  Copyright (C) 1992-2004 Free Software Foundation, Inc.
-  
+  Copyright (C) 2000 - 2004 Pawel A. Gajda <mis@pld.org.pl>
+
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2, or (at your option)
-  any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  it under the terms of the GNU General Public License, version 2 as
+  published by the Free Software Foundation (see file COPYING for details).
 
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
+
+/*
+  $Id$
+*/
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
 #include <stdlib.h>
 #include <sys/types.h>
@@ -59,7 +59,7 @@ void modify_environment(const struct passwd *pw)
   path = getenv("PATH");
   
 #if HAVE_CLEARENV
-  clearenv()
+  clearenv();
 #else
   extern char **environ;   
   environ = n_malloc(2 * sizeof (char *));
