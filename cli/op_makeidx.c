b12ac3454fdef358f76c6c69a6272d9da28ac02e
Paweł Gajda <mis@pld-linux.org>
2004-10-23 17:45:04 +0000

    - hide --compress - not usable yet; added --nocompress

--- cli/op_makeidx.c
+++ cli/op_makeidx.c
@@ -36,14 +36,15 @@
 #define OPT_MKIDXZ      (OPT_GID + 3) /* legacy */
 #define OPT_NODESC      (OPT_GID + 4)
 #define OPT_COMPR       (OPT_GID + 5)
-#define OPT_TYPE        (OPT_GID + 6)
-#define OPT_NODIFF      (OPT_GID + 7)
+#define OPT_NOCOMPR     (OPT_GID + 6)
+#define OPT_TYPE        (OPT_GID + 7)
+#define OPT_NODIFF      (OPT_GID + 8)
 
 /* The options we understand. */
 static struct argp_option options[] = {
 {0,0,0,0, N_("Repository index creation:"), OPT_GID},
 
-{"mkidx", OPT_MKIDX, "FILE", OPTION_ARG_OPTIONAL,
+{"mkidx", OPT_MKIDX, "PATH", OPTION_ARG_OPTIONAL,
  N_("Create package index (SOURCE-PATH/packages.dir by default)"), OPT_GID},
 
 {"makeidx", OPT_MAKEIDX, 0, OPTION_ALIAS, 0, OPT_GID }, 
@@ -52,7 +53,7 @@ static struct argp_option options[] = {
      N_("Sets the index type (use --stl to list available values)"),
      OPT_GID },
 
-{"mkidxz", OPT_MKIDXZ, "FILE", OPTION_ARG_OPTIONAL | OPTION_HIDDEN,
+{"mkidxz", OPT_MKIDXZ, "PATH", OPTION_ARG_OPTIONAL | OPTION_HIDDEN,
  N_("Likewise, but gzipped file is created"), OPT_GID},
 
 {"nodesc", OPT_NODESC, 0, 0,
@@ -62,9 +63,15 @@ static struct argp_option options[] = {
 {"nodiff", OPT_NODIFF, 0, 0,
  N_("Don't create index delta files"), OPT_GID },
 
-{"compress", OPT_COMPR, "type", 0, 
+{"nocompress", OPT_NOCOMPR, NULL, 0, 
+ N_("Create uncompressed index"), OPT_GID },
+
+{"compress", OPT_COMPR, "type", OPTION_HIDDEN, /* not finished yet */
  N_("Sets compression type (none, bz2, gz)"), OPT_GID },
 { 0, 0, 0, 0, 0, 0 },
+
+
+{ 0, 0, 0, 0, 0, 0 },    
 };
 
 struct arg_s {
@@ -73,7 +80,7 @@ struct arg_s {
     struct poldek_ctx   *ctx;
     struct source       *src_mkidx;
     char                *idx_type;
-    char                *idx_compress;
+    tn_hash             *opts;
 };
 #define DO_MAKEIDX (1 << 0)
 
@@ -108,7 +115,7 @@ static void arg_s_free(void *a)
     }
 
     n_cfree(&arg_s->idx_type);
-    n_cfree(&arg_s->idx_compress);
+    n_hash_free(arg_s->opts);
     free(arg_s);
 }
 
@@ -128,8 +135,9 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
     } else {
         arg_s = n_calloc(1, sizeof(*arg_s));
         arg_s->crflags = arg_s->cnflags = 0;
-        arg_s->idx_type = arg_s->idx_compress = NULL;
+        arg_s->idx_type = NULL;
         arg_s->src_mkidx = NULL;
+        arg_s->opts = n_hash_new(16, free);
         arg_s->ctx = rt->ctx;
         rt->_opdata = arg_s;
         rt->_opdata_free = arg_s_free;
@@ -161,7 +169,11 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
             break;
             
         case OPT_COMPR:
-            arg_s->idx_compress = n_strdup(arg);
+            n_hash_replace(arg_s->opts, "compress", n_strdup(arg));
+            break;
+
+        case OPT_NOCOMPR:
+            n_hash_replace(arg_s->opts, "compress", n_strdup("none"));
             break;
            
         default:
@@ -228,20 +240,20 @@ static int make_idx(struct arg_s *arg_s)
         MEMINF("before mkidx");
 
         if (types == NULL) {     /* no types  */
-            msgn(2, "Making index of %s (type=%s)...", source_idstr(src),
+            msgn(3, "Making index of %s (type=%s)...", source_idstr(src),
                  src->type);
-            if (!source_make_idx(src, NULL, NULL, path, arg_s->crflags))
+            if (!source_make_idx(src, NULL, NULL, path, arg_s->crflags,
+                                 arg_s->opts))
                 nerr++;
             
         } else
             for (j = 0; j < n_array_size(types); j++) {
                 const char *dtype = n_array_nth(types, j);
-                if (j > 0)
-                    msg(1, "\n");
-                msgn(1, "Making '%s' index of %s (type=%s)...", dtype,
+                msgn(3, "Making '%s' index of %s (type=%s)...", dtype,
                      source_idstr(src), src->type);
                 MEMINF("before");
-                if (!source_make_idx(src, NULL, dtype, path, arg_s->crflags))
+                if (!source_make_idx(src, NULL, dtype, path, arg_s->crflags,
+                                     arg_s->opts))
                     nerr++;
                 MEMINF("after");
             }
