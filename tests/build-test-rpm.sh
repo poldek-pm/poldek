9e3293dd0537cbd628c72ef4cff8a24f60670291
Paweł Gajda <mis@pld-linux.org>
2006-07-09 21:20:06 +0000

    - %%files supported

--- tests/build-test-rpm.sh
+++ tests/build-test-rpm.sh
@@ -7,6 +7,12 @@ version=
 release="1"
 requires=
 provides=
+files=
+
+usage_and_exit() {
+    echo "Usage $0 -n NAME -v VERSION-[RELEASE [-p PROVIDES] [-r REQUIRES] [-f file] "
+    exit 1
+}
 
 while test $# -gt 0 ; do
     case "${1}" in
@@ -22,12 +28,15 @@ while test $# -gt 0 ; do
         -r)
             shift; requires="$requires ${1},"; shift ;;
 
+        -f)
+            shift; files="$files ${1} "; shift ;;
+
         *)
             echo "unknown option ${1}"; exit 1;
     esac
 done
 
-[ -n "$name" -a -n "$version" ] || exit 1;
+[ -n "$name" -a -n "$version" ] || usage_and_exit;
 
 if echo $version | grep -q '-'; then
     release=$(echo $version | cut -f 2 -d -)
@@ -45,11 +54,30 @@ echo "Summary: $name" >> $SPEC
 echo "Group: System" >> $SPEC
 echo "License: foo" >> $SPEC
 echo "BuildArch: noarch" >> $SPEC
+echo "BuildRoot: /tmp/%{name}-%{version}-root-%(id -u -n)" >> $SPEC
 [ -n "$provides" ] &&  echo "Provides: $provides" >> $SPEC
 [ -n "$requires" ] && echo "Requires: $requires" >> $SPEC
 
 echo -e "%description\n$name" >> $SPEC
+echo -e "%prep\n%pre\n" >> $SPEC
 
-echo -e "%prep\n%pre\n%files\n%defattr(644,root,root,755)" >> $SPEC
-echo -e "%clean\nrm -rf $$RPM_BUILD_ROOT" >> $SPEC
+if [ -n "$files" ]; then
+    echo "%install" >> $SPEC
+    for f in $files; do
+        dn=$(dirname $f)
+        echo "mkdir -p \$RPM_BUILD_ROOT/$dn" >> $SPEC
+        echo "touch \$RPM_BUILD_ROOT/$f" >> $SPEC
+    done
+fi
+
+echo -e "%files\n%defattr(644,root,root,755)" >> $SPEC
+if [ -n "$files" ]; then
+    for f in $files; do
+        dn=$(dirname $f)
+        echo "%dir $dn" >> $SPEC
+        echo "$f" >> $SPEC
+    done
+fi
+
+echo -e "%clean\nrm -rf \$RPM_BUILD_ROOT" >> $SPEC
 rpmbuild -bb $SPEC
