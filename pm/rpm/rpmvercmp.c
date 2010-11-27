a85d35e0ee7946c3ae0a447eee83d1a95cb76271
Marcin Banasiak <marcin.banasiak@gmail.com>
2010-11-27 23:51:53 +0100

    rpmvercmp: rewritten to use rpmEVRcompare()

--- pm/rpm/rpmvercmp.c
+++ pm/rpm/rpmvercmp.c
@@ -4,45 +4,60 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "pm_rpm.h"
-#include "i18n.h"
+#define _RPMEVR_INTERNAL
+#include <rpm/rpmevr.h>
+
+static void parse(const char *evrstr, EVR_t evr)
+{
+    rpmEVRparse(evrstr, evr);
+    
+    if (evr->E == NULL) {
+	evr->E = "0";
+	evr->Elong = 0;
+    }
+    
+    if (evr->R == NULL)
+	evr->R = "0";
+}
 
 int main(int argc, char *argv[])
 {
-    int cmprc, is_test = 0;
+    int cmprc;
     const char *v1, *v2;
-
+    EVR_t evr1, evr2;
     
     if (argc < 3) {
-        printf("Usage: rpmvercmp [-t] VERSION1 VERSION2\n");
+        printf("Usage: rpmvercmp VERSION1 VERSION2\n");
         exit(EXIT_SUCCESS);
     }
 
     if (argc == 3) {
         v1 = argv[1];
         v2 = argv[2];
-        
-    } else if (argc == 4 && strcmp(argv[1], "-t") == 0) {
-        v1 = argv[2];
-        v2 = argv[3];
-        is_test = 1;
-        
+    
     } else {
-        printf("Usage: rpmvercmp [-t] VERSION1 VERSION2\n");
+        printf("Usage: rpmvercmp VERSION1 VERSION2\n");
         exit(1);
     }
 
+    evr1 = malloc(sizeof(struct EVR_s));
+    evr2 = malloc(sizeof(struct EVR_s));
     
-    cmprc = rpmvercmp(v1, v2);
+    parse(v1, evr1);
+    parse(v2, evr2);
+    
+    cmprc = rpmEVRcompare(evr1, evr2);
+    
     printf("%s %s %s\n", v1, cmprc == 0 ?  "==" : cmprc > 0 ? ">" : "<", v2);
-    if (is_test) {
-        cmprc = rpmvercmp(v2, v1);
-        printf("%s %s %s\n", v2, cmprc == 0 ?  "==" : cmprc > 0 ? ">" : "<", v1);
-    }
     
     if (cmprc < 0)
         cmprc = 2;
+
+    free((char *)evr1->str);
+    free((char *)evr2->str);
+    free(evr1);
+    free(evr2);
+
     exit(cmprc);
 }
