ec43fec423b9e577d7b5698185fb3b993dab57ed
Marcin Banasiak <marcin.banasiak@gmail.com>
2012-08-31 15:48:52 +0200

    Adjust autogen.sh and make snap for git

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# $Id$
 
 if [ ! -f capreq.h ]; then
     cd `dirname $0`;
@@ -19,16 +18,10 @@ runcmd () {
 }
 
 CONFOPTS="--enable-maintainer-mode --enable-compile-warnings"
-
-getlib_mode="link"
-if [ -n "$1" -a "$1" = "makedist" ]; then
-    rm -f trurlib tndb
-    getlib_mode="cp"
-    shift
-fi
 CONFOPTS="$CONFOPTS $@"
-runcmd ./getlib.sh trurlib $getlib_mode
-runcmd ./getlib.sh tndb    $getlib_mode
+
+runcmd git submodule init
+runcmd git submodule update
 
 # generate po/POTFILES.in
 make -f Makefile.extra POTFILES_in
