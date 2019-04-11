4ebdbc3ccb39dc1d44fc66a42326a19a296dce61
mis <mis@pld-linux.org>
2019-04-11 10:45:21 +0200

    fix gcc warn

--- vfile/vfffmod.c
+++ vfile/vfffmod.c
@@ -41,16 +41,16 @@ struct vf_module vf_mod_vfff = {
     "vfff",
     VFURL_HTTP | VFURL_FTP,
     do_init,
-    do_destroy, 
+    do_destroy,
     do_retr,
-    do_stat, 
+    do_stat,
     0
 };
 
 
 static tn_list *vcn_pool = NULL;               /* connections */
 
-static 
+static
 void do_vlog(const char *fmt, va_list ap)
 {
     vf_vlog(VFILE_LOG_INFO, fmt, ap);
@@ -67,24 +67,24 @@ int do_init(void)
 }
 
 static
-void do_destroy(void) 
+void do_destroy(void)
 {
     n_list_free(vcn_pool);
     vcn_pool = NULL;
 }
 
 
-static int toremove_cn_fakecmp(const void *a, const void *b) 
+static int toremove_cn_fakecmp(const void *a, const void *b)
 {
     const struct vcn *cn = a;
 
     b = b;
-    if (cn->state != VCN_ALIVE) 
+    if (cn->state != VCN_ALIVE)
         return 0;
     return -1;
 }
 
-void vcn_pool_vacuum(void) 
+void vcn_pool_vacuum(void)
 {
     n_list_remove_ex(vcn_pool, NULL, toremove_cn_fakecmp);
 }
@@ -95,8 +95,8 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
     struct vcn         *cn;
     char               *host, *login = NULL, *passwd = NULL;
     int                port, vcn_proto = 0;
-    
-    
+
+
     host = req->host;
     port = req->port;
     login = req->login;
@@ -104,13 +104,13 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
 
     if (strcmp(req->proto, "http") == 0)
         vcn_proto = VCN_PROTO_HTTP;
-    
+
     else if (req->proxy_proto && strcmp(req->proxy_proto, "http") == 0)
         vcn_proto = VCN_PROTO_HTTP;
-    
+
     else if (strcmp(req->proto, "ftp") == 0)
         vcn_proto = VCN_PROTO_FTP;
-    
+
     else
         n_assert(0);
 
@@ -123,24 +123,24 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
             if (port <= 0)
                 port = IPPORT_HTTP;
             break;
-            
+
         case VCN_PROTO_FTP:
             if (login == NULL)
                 login = "anonymous";
-        
+
             if (passwd == NULL)
                 passwd = vfile_conf.anon_passwd;
 
             n_assert(passwd);
-        
+
             if (req->proxy_host) {
                 int len;
                 char *s;
-                
+
                 len = strlen(login) + 1 + strlen(req->host) + 1;
                 s = alloca(len);
                 n_snprintf(s, len, "%s@%s", login, req->host);
-                
+
                 login = s;
                 host = req->proxy_host;
                 port = req->proxy_port;
@@ -149,14 +149,14 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
             if (port <= 0)
                 port = IPPORT_FTP;
             break;
-            
+
         default:
             n_assert(0);
     }
 
     if (vcn_pool == NULL)
         do_init();
-    
+
     vcn_pool_vacuum();
     n_list_iterator_start(vcn_pool, &it);
     while ((cn = n_list_iterator_get(&it))) {
@@ -167,7 +167,7 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
             if (cn->login) {
                 if (login == NULL || strcmp(cn->login, login) != 0)
                     continue;
-                
+
             } else if (login) {
                 continue;
             }
@@ -175,14 +175,14 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
             if (cn->passwd) {
                 if (passwd == NULL || strcmp(cn->passwd, passwd) != 0)
                     continue;
-                
+
             } else if (passwd) {
                 continue;
             }
 
             if (!vcn_is_alive(cn))
                 continue;
-            
+
             if (*vfile_verbose > 1)
                 vf_loginfo("Reusing connection %s%s%s:%d\n",
                             cn->login ? cn->login : "",
@@ -191,14 +191,14 @@ static struct vcn *vcn_pool_do_connect(struct vf_request *req)
             break;
         }
     }
-    
+
     if (cn == NULL) {
         cn = vcn_new(vcn_proto, host, port, login, passwd,
                      req->proxy_login, req->proxy_passwd);
         if (cn)
             n_list_push(vcn_pool, cn);
     }
-    
+
     return cn;
 }
 
@@ -213,7 +213,7 @@ struct do_fn {
 static void set_err(struct vf_request *req, int err_no, const char *fmt, ...)
 {
     va_list args;
-    
+
     va_start(args, fmt);
     vf_vlog(VFILE_LOG_ERR, fmt, args);
     va_end(args);
@@ -227,7 +227,7 @@ int do_vfn(const struct do_fn *dofn, struct vf_request *req,
     struct vcn        *cn;
     struct vfff_req   vreq;
     int                rc;
-    
+
     vfff_verbose = vfile_verbose;
     req->req_errno = 0;
 
@@ -241,7 +241,7 @@ int do_vfn(const struct do_fn *dofn, struct vf_request *req,
 
     memset(&vreq, 0, sizeof(vreq));
     vreq.uri = req->proxy_host ? req->url : req->uri;
-    
+
     if (req->dest_fd > 0) {
         vreq.out_path = req->destpath;
         vreq.out_fd = req->dest_fd;
@@ -251,27 +251,26 @@ int do_vfn(const struct do_fn *dofn, struct vf_request *req,
             vreq.progress_fn = vf_progress;
         }
     }
-    
+
     *vreq.redirected_to = '\0';
-    
+
     if ((rc = dofn->fn(cn, &vreq))) {
         req->st_remote_mtime = vreq.st_remote_mtime;
         req->st_remote_size = vreq.st_remote_size;
-        
+
     } else if (*vreq.redirected_to) {
-        char topath[PATH_MAX], *topathp = vreq.redirected_to;
+        char topath[PATH_MAX + 128], *topathp = vreq.redirected_to;
         int  foreign_proto = 0;
 
         n_assert(cn->proto == VCN_PROTO_HTTP);
-        
+
         if (*vreq.redirected_to == '/') {
             snprintf(topath, sizeof(topath), "http://%s%s", req->host,
                      vreq.redirected_to);
             topathp = topath;
-            
-        } else if (strncmp(vreq.redirected_to, "http://", 7) != 0) 
+        } else if (strncmp(vreq.redirected_to, "http://", 7) != 0)
             foreign_proto = 1;
-        
+
         if (topathp && vf_request_redirto(req, topathp)) {
             rc = 0;
             if (foreign_proto == 0)
@@ -282,7 +281,7 @@ int do_vfn(const struct do_fn *dofn, struct vf_request *req,
             rc = 0;
         }
     }
-    
+
     return rc;
 }
 
@@ -291,16 +290,16 @@ int do_retr(struct vf_request *req)
 {
     struct do_fn dofn;
     int rc;
-    
+
     dofn.type = DO_RETR;
     dofn.fn = vcn_retr;
-    
+
     if (!(rc = do_vfn(&dofn, req, 0))) {
         req->req_errno = vfff_errno;
         if ((req->flags & VF_REQ_INT_REDIRECTED) == 0)
             vf_logerr("%s: %s\n", vf_mod_vfff.vfmod_name, vfff_errmsg());
     }
-    
+
     return rc;
 }
 
@@ -309,7 +308,7 @@ int do_stat(struct vf_request *req)
 {
     struct do_fn dofn;
     int rc;
-    
+
     dofn.type = DO_STAT;
     dofn.fn = vcn_stat;
 
@@ -318,6 +317,6 @@ int do_stat(struct vf_request *req)
         if ((req->flags & VF_REQ_INT_REDIRECTED) == 0)
             vf_logerr("%s: %s\n", vf_mod_vfff.vfmod_name, vfff_errmsg());
     }
-    
+
     return rc;
 }
