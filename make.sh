8a01ed0477788f6dc9b7b6ab85fbf8fe9d5e72d7
mis <mis@pld-linux.org>
2019-04-15 13:15:51 +0200

    build on fedora (internal script)

--- make.sh
+++ make.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+# development helper script
 
 cd $(dirname $0) || exit 1
-#make LDFLAGS="-Wl,--as-needed" 
-make CFLAGS="-fno-builtin-log -O0 -g -Wall -W -pipe -std=gnu99" $@
+#make LDFLAGS="-Wl,--as-needed"
+
+CFLAGS="-fno-builtin-log -O0 -g -Wall -W -Wextra -Wformat-security -Wshadow -pipe -std=gnu99 -D_GNU_SOURCE=1"
+
+if [ "$(whoami)" != "mis" ]; then
+    make CFLAGS="$CFLAGS" $@
+
+elif grep -q "HAVE_RPMORG 1" config.h; then
+    echo "*** build on Fedora ***"
+    unset LC_CTYPE
+    [ -f ~/.a ] && . ~/.a
+    ssh mis@10.1.1.177 "cd poldek && make CFLAGS='$CFLAGS' $@"
+else
+    make CFLAGS="$CFLAGS" $@
+fi
