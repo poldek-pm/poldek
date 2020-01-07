5cd51ff5b262e37fab22fc594f6eb85915119635
mis <mis@pld-linux.org>
2020-01-07 16:56:25 +0100

    chore: optimized build

--- make.sh
+++ make.sh
@@ -4,7 +4,14 @@
 cd $(dirname $0) || exit 1
 #make LDFLAGS="-Wl,--as-needed"
 
-CFLAGS="-fno-builtin-log -O0 -g -Wall -W -Wextra -Wformat-security -Wshadow -pipe -std=gnu99 -D_GNU_SOURCE=1 -fbounds-check -Wformat -Werror=format-security"
+OPT="-O0"
+
+if [ "$1" == "profiled" ]; then
+   OPT="-O2"
+   shift
+fi
+
+CFLAGS="-fno-builtin-log $OPT -g -Wall -W -Wextra -Wformat-security -Wshadow -pipe -std=gnu99 -D_GNU_SOURCE=1 -fbounds-check -Wformat -Werror=format-security"
 
 if [ "$(whoami)" != "mis" ]; then
     make CFLAGS="$CFLAGS" $@
