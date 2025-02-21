88672bfbd2afa67c490c5d8a9ae37d43c08d78e4
mis <mistoo@gmail.com>
2025-02-21 14:22:25 +0100

    chore: internals up

--- make.sh
+++ make.sh
@@ -7,10 +7,11 @@ cd $(dirname $0) || exit 1
 OPT="-O0"
 
 if [ "$1" == "profiled" ]; then
-   OPT="-O2"
+    #OPT="-O2 "
+   OPT="$OPT -pg -static"
    shift
 fi
 
 CFLAGS="-fno-builtin-log $OPT -g -Wall -W -Wextra -Wformat-security -Wshadow -pipe -std=gnu99 -D_GNU_SOURCE=1 -fbounds-check -Wformat -Werror=format-security"
 
-make CFLAGS="$CFLAGS" $@
+make CFLAGS="$CFLAGS -DWITH_TIMETHIS=1 -DWITH_THREADS=1" $@
