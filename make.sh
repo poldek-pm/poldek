54861df4cecd3c26a71b1b866e07d01d7b37030d
mis <mistoo@gmail.com>
2025-04-04 12:07:33 +0200

    chore: sanitize compilation

--- make.sh
+++ make.sh
@@ -12,6 +12,18 @@ if [ "$1" == "profiled" ]; then
    shift
 fi
 
+LDFLAGS=""
+if [ "$1" == "gprofiled" ]; then
+   LDFLAGS="-ltcmalloc -lprofiler"
+   shift
+fi
+
+
+if [ "$1" == "sane" ]; then
+    OPT="-fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment"
+    shift
+fi
+
 CFLAGS="-fno-builtin-log $OPT -g -Wall -W -Wextra -Wformat-security -Wshadow -pipe -std=gnu99 -D_GNU_SOURCE=1 -fbounds-check -Wformat -Werror=format-security"
 
-make CFLAGS="$CFLAGS -DWITH_TIMETHIS=1" $@
+make CFLAGS="$CFLAGS -DWITH_TIMETHIS=1" LDADD="$LDFLAGS" $@ && ./cli/poldek -l >/dev/null
